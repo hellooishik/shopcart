b68734ee3ffef47eb1e490dfe9277fdab151bdc5
Oishik dutta <129034058+hellooishik@users.noreply.github.com>
2024-02-12 18:44:09 +0530

    Navbar Addded

--- src/App.js
+++ src/App.js
@@ -1,12 +1,96 @@
 import React from "react";
 import Cart from "./Components/cart";
+import Navbar from "./Components/navbar";
 import "./index.css";
-function App() {
-  return (
-    <div className="App">
-      <Cart />
-    </div>
-  );
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      products: [
+        {
+          price: 99,
+          title: "Watch",
+          qty: 1,
+          img: "",
+          id: 1,
+        },
+        {
+          price: 999,
+          title: "Mobile Phone",
+          qty: 10,
+          img: "",
+          id: 2,
+        },
+        {
+          price: 999,
+          title: "Laptop",
+          qty: 4,
+          img: "",
+          id: 3,
+        },
+      ],
+    };
+  }
+
+  handleDecreaseQuantity = (product) => {
+    console.log("Please Decrease the Quantity", product);
+    const { products } = this.state;
+    let index = products.indexOf(product);
+
+    products[index].qty -= 1;
+    if (products[index].qty <= 0) {
+      products.splice(index, 1);
+      alert(
+        "The quantity of " +
+          product.title +
+          " has reached zero and has been removed from your Cart"
+      );
+    }
+
+    this.setState({ products: [...products] });
+  };
+
+  handleIncreaseQuantity = (product) => {
+    console.log("Please Increase the quantity", product);
+    const { products } = this.state;
+    const index = products.indexOf(product);
+
+    products[index].qty += 1;
+    this.setState({
+      products: [...products],
+    });
+  };
+
+  handleDelection = (id) => {
+    const { products } = this.state;
+    const newProducts = products.filter((item) => item.id !== id);
+    this.setState({ products: newProducts });
+  };
+
+  getCartCount = () => {
+    const { products } = this.state;
+    let count = 0;
+    products.forEach((product) => {
+      count += product.qty;
+    });
+    return count;
+  };
+
+  render() {
+    const { products } = this.state;
+    return (
+      <div className="App">
+        <Navbar count={this.getCartCount()} />
+        <Cart
+          products={products}
+          onDecreaseQuantity={this.handleDecreaseQuantity}
+          onIncreaseQuantity={this.handleIncreaseQuantity}
+          onDeleteProduct={this.handleDelection}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Components/cart.js
+++ src/Components/cart.js
@@ -1,87 +1,21 @@
 import React from "react";
 import CartItem from "./cartItem";
 
-class Cart extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      products: [
-        {
-          price: 99,
-          title: "Watch",
-          qty: 1,
-          img: "",
-          id: 1,
-        },
-        {
-          price: 999,
-          title: "Mobile Phone",
-          qty: 10,
-          img: "",
-          id: 2,
-        },
-        {
-          price: 999,
-          title: "Laptop",
-          qty: 4,
-          img: "",
-          id: 3,
-        },
-      ],
-    };
-  }
-
-  // Function To handle the Decrese Quantity
-  // this file is based on cart item Decrease method
-  handleDecreaseQuantity = (product) => {
-    console.log("Please Decrease the Quantity", product);
-    const { products } = this.state;
-    let index = products.indexOf(product);
-
-    // Logical Implementation
-    products[index].qty -= 1;
-    if (products[index].qty <= 0) {
-      // Remove the item from the cart
-      products.splice(index, 1);
-      alert(
-        "The quantity of " +
-          product.title +
-          " has reached zero and has been removed from your Cart"
-      );
-    }
-
-    // Update the state
-    this.setState({ products: [...products] });
-  };
-
-  // This file is loced in the function of HandleIncrese quantity
-  handleIncreaseQuantity = (product) => {
-    console.log("Please Increase the quantity", product);
-    const { products } = this.state;
-    const index = products.indexOf(product);
-
-    // Finding the leap index
-    products[index].qty += 1;
-    this.setState({
-      products: [...products],
-    });
-  };
-
-  render() {
-    const { products } = this.state;
-    return (
-      <div className="cart">
-        {products.map((product) => (
-          <CartItem
-            product={product}
-            key={product.id}
-            onDecreaseQuantity={this.handleDecreaseQuantity}
-            onIncreaseQuantity={this.handleIncreaseQuantity}
-          />
-        ))}
-      </div>
-    );
-  }
-}
+const Cart = (props) => {
+  const { products } = props;
+  return (
+    <div className="cart">
+      {products.map((product) => (
+        <CartItem
+          product={product}
+          key={product.id}
+          onDecreaseQuantity={props.onDecreaseQuantity}
+          onIncreaseQuantity={props.onIncreaseQuantity}
+          onDeleteProduct={props.onDeleteProduct}
+        />
+      ))}
+    </div>
+  );
+};
 
 export default Cart;
